b8b5a248d3b378d7063b742beaa71dfceb3d14c8
reham rafaat <62518293+rehamrafaat202@users.noreply.github.com>
2021-07-21 00:13:28 +0200

    Update candidate_profile_screen.dart
    change profile image position

--- lib/views/screens/candidate_profile_screen.dart
+++ lib/views/screens/candidate_profile_screen.dart
@@ -21,6 +21,7 @@ class CandidateProfileScreenState extends State<CandidateProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     // get candidate index
     final indexes = ModalRoute.of(context).settings.arguments as List<dynamic>;
     final String trackID = indexes[0] as String;
@@ -47,10 +48,11 @@ class CandidateProfileScreenState extends State<CandidateProfileScreen> {
             // candidate image
             Container(
               width: double.infinity,
-              height: 200.0,
+              height: size.height * 0.3,
               decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: NetworkImage(candidate.img), fit: BoxFit.fill),
+                color: Style.primaryColor,
+                // image: DecorationImage(
+                //     image: NetworkImage(candidate.img), fit: BoxFit.fill),
                 borderRadius: BorderRadius.only(
                   bottomRight: Radius.circular(20),
                   bottomLeft: Radius.circular(20),
@@ -65,58 +67,90 @@ class CandidateProfileScreenState extends State<CandidateProfileScreen> {
                 width: double.infinity,
                 child: ProgramWidget(candidate.program)),
             // name of candidate and his social accounts
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 150, horizontal: 15),
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: Style.lightColor,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Style.primaryColor,
-                      spreadRadius: 2,
-                      blurRadius: 3,
-                      offset: Offset(0, 0),
-                    )
+            Positioned(
+              top: 70,
+              right: 20,
+              left: 20,
+              child: Container(
+                height: 230,
+                // margin: EdgeInsets.symmetric(vertical: 150, horizontal: 15),
+                // padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Style.lightColor,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Style.primaryColor,
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: Offset(0, 0),
+                      )
+                    ],
+                    border: Border.all(width: 1, color: Colors.grey)),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(20),
+                      width: 130,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        image: DecorationImage(
+                            image: NetworkImage(candidate.img),
+                            fit: BoxFit.fill),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.only(top: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            // name of the candidate
+                            Text(
+                              candidate.name,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 22,
+                                color: Style.darkColor,
+                              ),
+                              overflow: TextOverflow.clip,
+                              softWrap: true,
+                            ),
+                            SizedBox(
+                              height: 6,
+                            ),
+                            //his track
+                            Text(
+                              CandidatesModel.tracks[trackID].name,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Style.secondColor,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.left,
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            // his social accounts
+                            Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  SocialWidget(
+                                      img: 'facebook',
+                                      url: candidate.facebookUrl),
+                                  SocialWidget(
+                                      img: 'twitter', url: candidate.twitterUrl)
+                                ]),
+                          ],
+                        ),
+                      ),
+                    ),
                   ],
-                  border: Border.all(width: 1, color: Colors.grey)),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // name of the candidate
-                  Text(
-                    candidate.name,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22,
-                      color: Style.darkColor,
-                    ),
-                    overflow: TextOverflow.clip,
-                    softWrap: true,
-                  ),
-                  SizedBox(
-                    height: 6,
-                  ),
-                  //his track
-                  Text(
-                    CandidatesModel.tracks[trackID].name,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Style.secondColor,
-                    ),
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.left,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  // his social accounts
-                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                    SocialWidget(img: 'facebook', url: candidate.facebookUrl),
-                    SocialWidget(img: 'twitter', url: candidate.twitterUrl)
-                  ]),
-                ],
+                ),
               ),
             ),
           ]),
